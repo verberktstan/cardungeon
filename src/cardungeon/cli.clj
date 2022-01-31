583bfa2e6e8def31bbf5a7b40b9e9b8fc14ea883
verberktstan <stan@brightin.nl>
2022-01-31 11:54:40 +0100

    Adde base deck, shuffle and deal, improve cli printing

--- src/cardungeon/cli.clj
+++ src/cardungeon/cli.clj
@@ -3,19 +3,22 @@
             [clojure.edn :as edn]
             [clojure.string :as str]))
 
+(defn- print-dungeon [dungeon]
+  (-> dungeon
+      (update :dungeon/discarded count)
+      (update :dungeon/draw-pile count)
+      println))
+
 (defn -main [& _]
   (newline)
   (println "Welcome to Cardungeon!")
   (println "type 'exit' to stop the game.")
   (println "To play, enter the number of the dungeon room card to play it.")
-  (loop [game {:player/health 13
-               :dungeon/room
-               {0 {:card/monster 2}
-                1 {:card/monster 3}
-                2 {:card/potion 4}}}
+  (loop [game (game/new-game)
          input ""]
     (let [parsed-input (edn/read-string input)
-          new-game (some-> game (game/play parsed-input))]
+          new-game (or (game/play game parsed-input) game)
+          new-game (or (game/deal new-game) new-game)]
       (cond
         (#{"exit"} (str/lower-case input))
         (println "stopping game..")
@@ -26,4 +29,4 @@
           (println "You cleared the dungeon!"))
 
         :else
-        (recur (doto (or new-game game) println) (read-line))))))
+        (recur (doto (or new-game game) print-dungeon) (read-line))))))
--- src/cardungeon/core.clj
+++ src/cardungeon/core.clj
@@ -1,5 +1,12 @@
 (ns cardungeon.core)
 
+(def BASE_DECK
+  (let [monster-range (map #(if (<= % 10) % 10) (range 2 15))]
+    (concat
+     (map (partial assoc {} :card/monster) monster-range)
+     (map (partial assoc {} :card/monster) monster-range)
+     (map (partial assoc {} :card/potion) (range 2 11)))))
+
 (defn- fight
   "Returns the dungeon with monster's strength subtracted from player's health"
   [dungeon {:card/keys [monster]}]
@@ -25,6 +32,28 @@
           (update :dungeon/room dissoc room-idx)
           (play* card)
           (update :dungeon/discarded conj card)))))
+
+(defn- reshuffle [{:dungeon/keys [discarded] :as dungeon}]
+  (-> dungeon
+      (dissoc :dungeon/discarded)
+      (update :dungeon/draw-pile concat (shuffle discarded))))
+
+(defn- merge-room [room cards]
+  (let [free-indices (remove (partial contains? room) (range))]
+    (merge room (zipmap free-indices cards))))
+
+(defn deal [{:dungeon/keys [draw-pile room] :as dungeon}]
+  (when (#{0 1} (count room))
+    (let [n-cards (- 4 (count room))
+          drawn (take n-cards draw-pile)]
+      (-> dungeon
+          (update :dungeon/draw-pile (partial drop n-cards))
+          (update :dungeon/room merge-room drawn)))))
+
+(defn new-game []
+  (some-> {:player/health 20 :dungeon/discarded BASE_DECK}
+      reshuffle
+      deal))
 
 (defn finished?
   "Returns true when the dungeon's room and draw-pile are both empty."
