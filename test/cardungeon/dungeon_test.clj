138523010d3a51149e41a25619c6eb2373f4a264
verberktstan <stan@brightin.nl>
2022-02-01 16:31:33 +0100

    Fix card stuff in tests and health cap

--- test/cardungeon/dungeon_test.clj
+++ test/cardungeon/dungeon_test.clj
@@ -2,45 +2,46 @@
   (:require [clojure.test :refer [are deftest is testing]]
             [cardungeon.dungeon :as sut]
             [cardungeon.player :as player]
-            [cardungeon.room :as room]))
+            [cardungeon.room :as room]
+            [cardungeon.card :as card]))
 
 ;; Basic fighting interaction with monster cards
 (deftest fight-test
   (testing "fight"
     (testing "reduces player's health by monster strength"
       (are [dungeon monster] (= dungeon (#'sut/fight {::player/health 13} monster))
-        {::player/health 10} {:card/monster 3}
-        {::player/health 13} {:card/monster 0}
-        {::player/health -1} {:card/monster 14}))
+        {::player/health 10} {::card/monster 3}
+        {::player/health 13} {::card/monster 0}
+        {::player/health  0} {::card/monster 14}))
     (testing "asserts if supplied card is a monster"
-        (is (thrown? AssertionError (#'sut/fight {} {:card/potion 3}))))))
+        (is (thrown? AssertionError (#'sut/fight {} {::card/potion 3}))))))
 
 ;; Basic healing interaction with potion cards
 (deftest heal-test
   (testing "heal"
     (testing "increases the player's health by potion strength"
       (are [dungeon potion] (= dungeon (#'sut/heal {::player/health 8} potion))
-        (room/mark-already-healed {::player/health 13}) {:card/potion 5}
-        (room/mark-already-healed {::player/health 16}) {:card/potion 8}))
+        (room/mark-already-healed {::player/health 13}) {::card/potion 5}
+        (room/mark-already-healed {::player/health 16}) {::card/potion 8}))
     (testing "doesn't work if already-healed in this room"
       (is (= (room/mark-already-healed {})
              (#'sut/heal
               (room/mark-already-healed {})
-              {:card/potion 3}))))
+              {::card/potion 3}))))
     (testing "asserts if supplied card is a potion"
-      (is (thrown? AssertionError (#'sut/heal {} {:card/monster 3}))))))
+      (is (thrown? AssertionError (#'sut/heal {} {::card/monster 3}))))))
 
 (deftest play-test
   (testing "play"
     (let [game (-> {::player/health 13
-                    ::sut/room {0 {:card/monster 2}
-                                1 {:card/monster 3}}}
+                    ::sut/room {0 {::card/monster 2}
+                                1 {::card/monster 3}}}
                    (sut/play 0))]
       (testing "returns the game with the room card removed"
-        (is (= {1 {:card/monster 3}}
+        (is (= {1 {::card/monster 3}}
                (::sut/room game))))
       (testing "returns the game with the room card moved to discarded coll"
-        (is (= [{:card/monster 2}]
+        (is (= [{::card/monster 2}]
                (::sut/discarded game)))))
     (testing "returns nil if the room card isn't there"
       (is (nil? (sut/play {::sut/room {0 :card}} 10))))))
